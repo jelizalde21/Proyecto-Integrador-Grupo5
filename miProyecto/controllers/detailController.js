f5df5ab70f0de0a61d2d781e6a94ea7af87c3e39
jelizalde21 <63111904+jelizalde21@users.noreply.github.com>
2021-11-11 00:18:46 -0300

    Intentando hacer el detalle

--- miProyecto/controllers/detailController.js
+++ miProyecto/controllers/detailController.js
@@ -9,7 +9,28 @@ const detailController = {
         res.render ('detailUser', {users: data.lista, listaPosts: posts.lista})
     },
     add: function (req, res) {
-        res.render ('detailAdd')
+        if (req.session.usuario) {
+            res.render('detailAdd')
+        } else {
+            return res.redirect('/')
+        }
+    },
+    create: (req, res) => { 
+        if (req.session.usuario) { //chequea que haya un usuario logueado
+            db.Post.create({ //un producto con los siguientes datos
+                    usuario: req.body.usuario, //datos en el input
+                    portada: req.file.filename, 
+                    comentario: req.body.comentario,
+                    users_id: req.session.usuario.id //si está logueado, en users_id, va a poner el id del usuario logueado
+                })
+                .then(posteo => {
+                    res.redirect("/detail/id/" + posteo.id) //si está todo ok, nos manda al producto creado  
+                })
+        } else {
+            res.render('detailAdd', { //si no está todo ok, nos muestra la misma vista con los campos vacíos para que los llenemos
+                error: 'No se pueden dejar campos vacíos'
+            })
+        }
     },
     post: function (req, res) {
         let idPost = req.params.id;
