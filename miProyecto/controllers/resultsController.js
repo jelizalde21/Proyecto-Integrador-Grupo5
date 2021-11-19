b4daad11c4c8430ca90730c42b2de98d357075a2
eugeniagarciaborghes <egarciaborghes@udesa.edu.ar>
2021-11-19 17:01:14 -0300

    buscador usuarios

--- miProyecto/controllers/resultsController.js
+++ miProyecto/controllers/resultsController.js
@@ -7,24 +7,71 @@ const controller = {
         return res.send(Data.lista);
     },
     show: function (req, res) {
-       db.Post.findAll({
-           where:{
-               caption: {
-                   [ Op.like]: "%"+req.query.search+"%"
-               }
-           },
-           limit: 10,
-           order: [
-               ['fecha', 'DESC'],
-           ]
-       })
-       .then(posts => {
-        return res.render('results', {posts: posts})  
-       })         
-            
+        db.Post.findAll({
+                where: {
+                    caption: {
+                        [Op.like]: "%" + req.query.search + "%"
+                    }
+                },
+                include: [{
+                    all: true,
+                }],
+                limit: 10,
+                order: [
+                    ['fecha', 'DESC'],
+                ]
+            })
+            .then(posts => {
+                if (posts.length > 0) {
+                    return res.render('results', {
+                        posts: posts,
+                        resultado: 'Resultados de busqueda ' + req.query.search
+                    })
 
-        }
- 
+                } else {
+                    return res.render('results', {
+                        posts: posts,
+                        resultado: 'No existen resultados para ese criterio de busqueda'
+                    })
+                }
+
+            })
+
+
+    },
+
+    showUser: function (req, res) {
+        db.User.findAll({
+                where: {
+                    username: {
+                        [Op.like]: "%" + req.query.search + "%"
+                    }
+                },
+                include: [{
+                    all: true,
+                }],
+                
+            })
+            .then(users => {
+                if (users.length > 0) {
+                    return res.render('resultsUser', {
+                        users: users,
+                        resultado: 'Resultados de busqueda ' + req.query.search
+                    })
+
+                } else {
+                    return res.render('resultsUser', {
+                        users: users,
+                        resultado: 'No existen resultados para ese criterio de busqueda'
+                    })
+                }
+
+            })
+
+
+    }
+
 };
+
 
 module.exports = controller
--- miProyecto/routes/results.js
+++ miProyecto/routes/results.js
@@ -4,6 +4,6 @@ const controller = require('../controllers/resultsController');
 
 /* GET home page. */
 router.get('/', controller.index);
-router.get('/id/:id', controller.show);
-
+router.get('/search', controller.show);
+router.get('/searchUser', controller.showUser);
 module.exports = router;
