acc2b40c1ef0ff8362c3a5ca4405440d064003fe
horasbarbeito <hbarbeito@udesa.edu.ar>
2021-11-23 18:32:59 -0300

    Edit profile and more

--- miProyecto/controllers/profileController.js
+++ miProyecto/controllers/profileController.js
@@ -1,5 +1,7 @@
 const db = require('../database/models'); // relaciona controlador con modelos
 const Op = db.Sequelize.Op;
+const bcrypt = require('bcryptjs');
+
 
 const profileController = {
         index: (req, res) => { 
@@ -20,7 +22,43 @@ const profileController = {
                 })
         },
     edit: function (req, res) {
-        res.render ('profileEdit')
+        if(req.session.usuario){
+            db.User.findByPk(req.params.id)
+            .then(user =>{
+                res.render('profileEdit', {user: user})
+            })
+        }else{
+            return res.redirect('/')
+        }
+        
+    },
+
+    editUser: (req, res) => {
+        let passEncriptada = bcrypt.hashSync(req.body.passwords, 10);
+        let dateUpdated = new Date();
+        db.User.update({
+            nombre: req.body.nombre,
+            apellido: req.body.apellido,
+            email: req.body.email,
+            fecha: req.body.fecha,
+            username: req.body.usuario,
+            picture: req.file.filename,
+            passwords: passEncriptada,
+            updatedAt: dateUpdated
+        },
+        {
+            where: {id: req.params.id}
+        })
+        .then(profile =>{
+            /*res.send(profile)*/ 
+            res.redirect('/profile')
+        })
+        
+        .catch(err => {
+            console.log(err)
+            res.send(err)
+        })
+        
     }
 };
 
